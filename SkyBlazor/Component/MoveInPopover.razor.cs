b60c63f84b8c7927c87b0a5f2bfd5c412f1fd1ac
Z01440 <Z01440@ZYCZ01440>
2023-01-12 17:59:48 +0800

    添加项目文件。

---
+++ SkyBlazor/Component/MoveInPopover.razor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace SkyBlazor.Component
+{
+	public partial class MoveInPopover
+	{
+		[Inject]
+		private IJSRuntime JS { get; set; } = default!;
+		private IJSObjectReference? module;
+
+		public enum Direction
+		{
+			Left,
+			Right,
+			Top,
+			Bottom
+		}
+		
+		[Parameter]
+		public Direction PopDirection { get; set; } = Direction.Bottom;
+		//
+		// 摘要:
+		//		设置移入框部件
+		[Parameter]
+		[NotNull]
+		public RenderFragment? HeadContent { get; set; }
+
+		//
+		// 摘要:
+		//     获得弹出框部件
+		[Parameter]
+		[NotNull]
+		public RenderFragment? PopoverContent { get; set; }
+
+		//
+		// 摘要:
+		//     添加额外属性
+		[Parameter(CaptureUnmatchedValues = true)]
+		public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+		//
+		// 摘要:
+		//     点击事件
+		[Parameter]
+		public EventCallback<MouseEventArgs> MouseClickEvent { get; set; }
+
+		public async Task MouseMoveIn()
+		{
+			await module!.InvokeVoidAsync("AutoPopoverLocateShow", PopDirection);
+		}
+
+		public async Task MouseMoveOut()
+		{
+			await module!.InvokeVoidAsync("AutoPopoverLocateHidden", null);
+		}
+
+		protected override Task OnInitializedAsync()
+		{
+			return base.OnInitializedAsync();
+		}
+
+		protected override async Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				module = await JS.InvokeAsync<IJSObjectReference>("import", "../Common/MoveInPopover.razor.js");
+				await base.OnAfterRenderAsync(firstRender);
+			}
+		}
+	}
+}
---
+++ SkyBlazor/Component/MoveInPopover.razor.js
@@ -0,0 +1,135 @@
+﻿export function AutoPopoverLocateShow(direction) {
+    const currentClientHeight = window.innerHeight;
+    const currentClientWidth = window.innerWidth;
+
+    let popoverBlock = document.querySelector(".popover-block");
+    let userDefineBlock = document.querySelector(".user-define-block");
+    let popoverBlockStyle = popoverBlock.style;
+    popoverBlockStyle.display = "flex";
+    popoverBlockStyle.visibility = "hidden";
+
+    if (direction === 0) {
+        popoverBlockStyle.right = "100%";
+        userDefineBlock.style.flexDirection = "row-reverse";
+        let [offsetTop, offsetLeft] = GetOffsetByBody(popoverBlock);
+        let offsetRight = currentClientWidth - offsetLeft - popoverBlock.offsetWidth;
+        let offsetBottom = currentClientHeight - offsetTop - popoverBlock.offsetHeight;
+        if (offsetLeft < 0) {
+            if (offsetBottom < 0) {
+                if (offsetTop < 0) {
+                    if (offsetRight < 0) {
+                        return;
+                    }
+                    else {
+                        popoverBlockStyle.left = "100%";
+                        popoverBlockStyle.right = "auto";
+                        userDefineBlock.style.flexDirection = "row";
+                    }
+                }
+                else {
+                    popoverBlockStyle.left = 0;
+                    popoverBlockStyle.right = "auto";
+                    popoverBlockStyle.bottom = "100%";
+                    userDefineBlock.style.flexDirection = "column-reverse";
+                }
+            }
+            else {
+                popoverBlockStyle.left = 0;
+                popoverBlockStyle.right = "auto";
+                popoverBlockStyle.top = "100%";
+                userDefineBlock.style.flexDirection = "column";
+            }
+        }
+    }
+    if (direction === 1) {
+        popoverBlockStyle.left = "100%";
+        userDefineBlock.style.flexDirection = "row";
+        let [offsetTop, offsetLeft] = GetOffsetByBody(popoverBlock);
+        let offsetRight = currentClientWidth - offsetLeft - popoverBlock.offsetWidth;
+        let offsetBottom = currentClientHeight - offsetTop - popoverBlock.offsetHeight;
+
+        if (offsetRight < 0) {
+            if (offsetBottom < 0) {
+                if (offsetTop < 0) {
+                    if (offsetLeft < 0) {
+                        return;
+                    }
+                    else {
+                        popoverBlockStyle.left = "auto";
+                        popoverBlockStyle.right = "100%";
+                        userDefineBlock.style.flexDirection = "row-reverse";
+                    }
+                }
+                else {
+                    popoverBlockStyle.left = "auto";
+                    popoverBlockStyle.right = 0;
+                    popoverBlockStyle.bottom = "100%";
+                    userDefineBlock.style.flexDirection = "column-reverse";
+                }
+            }
+            else {
+                popoverBlockStyle.left = "auto";
+                popoverBlockStyle.right = 0;
+                popoverBlockStyle.top = "100%";
+                userDefineBlock.style.flexDirection = "column";
+
+            }
+        }
+    }
+    if (direction === 2) {
+        userDefineBlock.style.flexDirection = "column-reverse";
+        let [offsetTop, offsetLeft] = GetOffsetByBody(popoverBlock);
+        let offsetRight = currentClientWidth - offsetLeft - popoverBlock.offsetWidth;
+        
+        if (offsetRight < 0 && offsetLeft > 0) {
+            popoverBlockStyle.bottom = "100%";
+            popoverBlockStyle.right = 0;
+        }
+        if (offsetRight > 0 && offsetLeft < 0) {
+            popoverBlockStyle.bottom = "100%";
+            popoverBlockStyle.left = 0;
+        }
+        if (offsetRight > 0 && offsetLeft > 0) {
+            popoverBlockStyle.bottom = "100%";
+        }
+    }
+    if (direction === 3) {
+        userDefineBlock.style.flexDirection = "column";
+        let [offsetTop, offsetLeft] = GetOffsetByBody(popoverBlock);
+        let offsetRight = currentClientWidth - offsetLeft - popoverBlock.offsetWidth;
+
+        if (offsetRight < 0 && offsetLeft > 0) {
+            popoverBlockStyle.top = "100%";
+            popoverBlockStyle.right = 0;
+        }
+        if (offsetRight > 0 && offsetLeft < 0) {
+            popoverBlockStyle.top = "100%";
+            popoverBlockStyle.left = 0;
+        }
+        if (offsetRight > 0 && offsetLeft > 0) {
+            popoverBlockStyle.top = "100%";
+        }
+    }
+    popoverBlockStyle.visibility = "visible";
+}
+
+export function AutoPopoverLocateHidden() {
+    let popoverBlock = document.querySelector(".popover-block");
+    popoverBlock.style.display = "none";
+    popoverBlock.style.left = "auto";
+    popoverBlock.style.right = "auto";
+    popoverBlock.style.top = "auto";
+    popoverBlock.style.bottom = "auto";
+}
+
+function GetOffsetByBody(element) {
+    let offsetTop = 0;
+    let offsetLeft = 0;
+    while (element && element.tagName != "body") {
+        offsetTop += element.offsetTop;
+        offsetLeft += element.offsetLeft;
+        element = element.offsetParent;
+    }
+    
+    return [offsetTop, offsetLeft];
+}
